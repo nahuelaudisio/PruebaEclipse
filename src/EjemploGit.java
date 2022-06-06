3a68ebc306a83d7bd17e0a04e55779f88eb603a9
“Nahuel <nahuel.audisio.95@gmail.com>
2022-06-06 15:45:11 -0300

    agregue una variable

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -3,7 +3,9 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hola gente estoy programando en ECLIPSE");
+System.out.println("sigo con el curso de GIT");
+String nombre = "Nahuel Audisio";
 	}
 
 }
